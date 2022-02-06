077a8a18f9d2c4eed68c304b199e04c77a47e47d
faisalmujawar148 <faisal.mujawar4@gmail.com>
2022-02-06 17:34:33 +0400

    k

---
+++ about/javascript2.js
@@ -0,0 +1,52 @@
+var scrollToElement = function(el, ms){
+    var speed = (ms) ? ms : 600;
+    $('html,body').animate({
+        scrollTop: $(el).offset().top
+    }, speed);
+}
+
+$(document).ready(function() {
+  $('.nav-link').on('click', function(e) {
+    e.preventDefault();
+    var el = $(this).attr('href');
+    scrollToElement(el);
+  });
+
+  $(window).scroll(function() {
+    var x = $(window).scrollTop();
+
+    if (x >= 42) {
+      $("#navbar").fadeIn(300);
+    } else {
+      $("#navbar").fadeOut(300);
+    }
+
+  });
+});
+
+
+
+
+(function () {
+   'use strict';
+
+  // WAIT FOR ALL TO BE LOADED
+  window.onload = function() {
+
+    // add loaded class to html
+    var root = document.documentElement;
+    root.className += ' loaded';
+
+    // TYPING EFFECT
+    Typed.new('#typed', {
+            stringsElement: document.getElementById('typed-strings'),
+      loop: true,
+      typeSpeed: 7,
+      backSpeed: 2,
+      startDelay: 1000,
+      backDelay: 1200
+        });
+  }; // all loaded
+
+
+}());
